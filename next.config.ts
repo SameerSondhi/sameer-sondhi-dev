7b169221835ee1ce3b051e9fd633c4fa60f645a9
Sameer Sondhi <ssondhi@chirpn.com>
2025-11-23 18:48:36 -0800

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import withMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    reactStrictMode: true,
+    swcMinify: true,
 };
 
-export default nextConfig;
+export default withMDX({
+    extension: /\.mdx?$/,
+})(nextConfig);
+
+// export default nextConfig;
